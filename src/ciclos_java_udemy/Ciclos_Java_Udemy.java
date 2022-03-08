e156cb1025f1bf88b6ea5132904e31c23db56f8a
Rick <Rick@DESKTOP-N10MD3R>
2022-03-07 20:57:12 -0600

    Creación de proyecto

---
+++ src/ciclos_java_udemy/Ciclos_Java_Udemy.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ciclos_java_udemy;
+
+/**
+ *
+ * @author Rick
+ */
+public class Ciclos_Java_Udemy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Repositorio de prueba");
+    }
+    
+}
