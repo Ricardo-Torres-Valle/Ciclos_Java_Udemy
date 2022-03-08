4f33211fd5854cb42c6e2225d4a54a9ff34501ef
Rick <Rick@DESKTOP-N10MD3R>
2022-03-08 11:03:57 -0600

    43. Ejercicio Ciclo for

--- src/ciclos_java_udemy/Ciclos_Java_Udemy.java
+++ src/ciclos_java_udemy/Ciclos_Java_Udemy.java
@@ -16,11 +16,8 @@ public class Ciclos_Java_Udemy {
     public static void main(String[] args) {
         var contador = 0;
         
-        do{
-            System.out.println("contador = " + contador);
-            contador++;
-        }while(contador < 3);
-        
+        for(contador = 0; contador < 3; contador++)
+            System.out.println("Contador = "+contador);
     }
     
 }
